6ce51e4169c8dc213699a5c87cbfe63f2fdb4e56
JeffM20501 <kimmuna440@gmail.com>
2025-12-23 13:18:49 +0300

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open (f"{file_name}.txt", encoding="utf-8", mode="w") as txt_file:
+        txt_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open (f"{file_name}.txt", encoding='utf-8', mode='a') as txt_file:
+        txt_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open (f"{file_name}.txt", encoding='utf-8') as txt_file:
+        return txt_file.read()
